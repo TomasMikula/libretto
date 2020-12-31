e4628831df3c29b615ccee7507ae74eb124b2a51
Tomas Mikula <tomas.mikula@gmail.com>
2020-12-31 21:33:37 +0100

    Add delayNeed

--- src/main/scala/libretto/TimerDSL.scala
+++ src/main/scala/libretto/TimerDSL.scala
@@ -4,4 +4,15 @@ import scala.concurrent.duration.FiniteDuration
 
 trait TimerDSL extends CoreDSL {
   def delay(d: FiniteDuration): Done -⚬ Done
+
+  private val lib = CoreLib(this)
+  import lib._
+
+  def delayNeed(d: FiniteDuration): Need -⚬ Need = {
+    id                           [                      Need  ]
+      .introFst(lInvertSignal).to[ (Need |*|  Done) |*| Need  ]
+      .in.fst.snd(delay(d))   .to[ (Need |*|  Done) |*| Need  ]
+      .assocLR                .to[  Need |*| (Done  |*| Need) ]
+      .elimSnd(rInvertSignal) .to[  Need                      ]
+  }
 }
