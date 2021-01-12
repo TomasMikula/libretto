1d7aab9aa58d71f439683e3339b215c79e6bb0b2
Tomas Mikula <tomas.mikula@gmail.com>
2021-01-12 22:07:54 +0100

    Test Pollable.subscribeByKey

--- src/test/scala/libretto/StreamsTests.scala
+++ src/test/scala/libretto/StreamsTests.scala
@@ -5,6 +5,7 @@ import scala.concurrent.duration._
 class StreamsTests extends TestSuite {
   import kit.dsl._
   import kit.coreLib._
+  import kit.coreStreams._
   import kit.scalaLib._
   
   val scalaStreams = ScalaStreams(kit.dsl, kit.coreLib, kit.scalaLib, kit.coreStreams)
@@ -74,5 +75,38 @@ class StreamsTests extends TestSuite {
       assert(res1.toSet == Set(1, 2, 3, 7, 8, 9))
       assert(res2 == List(4, 5, 6))
     }
+  }
+  
+  test("subscribeByKey") {
+    import Pollable.subscribeByKey
+    import LSubscriber._
+
+    val byLength: Val[String] -⚬ (Val[Int] |*| Val[String]) =
+      liftV[String, (Int, String)](s => (s.length, s)) >>> liftPair
+      
+    val input: One -⚬ Pollable[String] =
+      Pollable.of("f", "fo", "foo", "fooo", "foooo", "pho", "phoo", "phooo", "bo", "boo")
+      
+    def subscription(k: Int): One -⚬ (Pollable[String] |*| (Val[Int] |*| Subscriber[String])) =
+      id                                     [                  One                                   ]
+        .>(lInvertLPollable(promise))     .to[ Pollable[String] |*|               Subscriber[String]  ]
+        .>.snd(introFst(const_(k)))       .to[ Pollable[String] |*| (Val[Int] |*| Subscriber[String]) ]
+
+    val subscriptions: One -⚬ (LList[Val[Int] |*| Subscriber[String]] |*| LList[Pollable[String]]) =
+      LList.of(subscription(3), subscription(4)) >>> LList.unzip >>> swap
+
+    val prg: One -⚬ Val[List[String]] =
+      parFromOne(input, subscriptions)    .to[  Pollable[String] |*| (    LList[Val[Int] |*| Subscriber[String]]  |*| LList[Pollable[String]]) ]
+        .>(|*|.assocRL)                   .to[ (Pollable[String] |*|      LList[Val[Int] |*| Subscriber[String]]) |*| LList[Pollable[String]]  ]
+        .>.fst.snd(LPollable.fromLList)   .to[ (Pollable[String] |*|  LPollable[Val[Int] |*| Subscriber[String]]) |*| LList[Pollable[String]]  ]
+        .>.fst(subscribeByKey(byLength))  .to[                   Done                                             |*| LList[Pollable[String]]  ]
+        .>.snd(Pollable.mergeAll)         .to[                   Done                                             |*|       Pollable[String]   ]
+        .>.snd(Pollable.toList)           .to[                   Done                                             |*|      Val[List[String]]   ]
+        .>.joinL                          .to[                                                                             Val[List[String]]   ]
+        
+    assertVal(
+      prg >>> liftV(_.toSet),
+      Set("foo", "fooo", "pho", "phoo", "boo"),
+    )
   }
 }
