331b52a4f3421af9a22fe952ca537956bfe53957
Tomas Mikula <tomas.mikula@gmail.com>
2021-03-10 00:03:07 +0100

    Adjust the font in Scaladoc code blocks to one that supports Unicode box drawing, namely Menlo.

--- scripts/scaladoc-snapshot.sh
+++ scripts/scaladoc-snapshot.sh
@@ -4,5 +4,14 @@ sbt doc
 git rm -rf docs/scaladoc/snapshot
 mkdir -p docs/scaladoc
 cp -R core/target/scala-3.0.0-RC1/api docs/scaladoc/snapshot
+
+# Edit stylesheet to use a font that correctly renders Unicode boxes (box-drawing characters).
+# TODO: Is there a better way to override the font used for code blocks?
+ed docs/scaladoc/snapshot/styles/scalastyle.css << END
+/--mono-font: "Fira Code"/
+s/--mono-font: "Fira Code"/--mono-font: "Menlo"/
+wq
+END
+
 git add docs/scaladoc/snapshot
 git commit
