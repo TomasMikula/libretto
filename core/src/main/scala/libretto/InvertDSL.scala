da5ab4fc8d5a9dc79dffece0fbb7aa2edcc7956a
Tomas Mikula <tomas.mikula@gmail.com>
2021-09-21 23:24:14 +0200

    Add contrapositive, die, dii to InvertDSL.

--- core/src/main/scala/libretto/InvertDSL.scala
+++ core/src/main/scala/libretto/InvertDSL.scala
@@ -13,4 +13,15 @@ trait InvertDSL extends ClosedDSL {
 
   override def curry[A, B, C](f: (A |*| B) -⚬ C): A -⚬ (B =⚬ C) =
     andThen(introFst(forevert[B]), andThen(assocLR, par(id, andThen(swap, f))))
+
+  def contrapositive[A, B](f: A -⚬ B): -[B] -⚬ -[A] =
+    andThen(introFst(andThen(forevert[A], snd(f))), andThen(assocLR, elimSnd(backvert[B])))
+
+  /** Double-inversion elimination. */
+  def die[A]: -[-[A]] -⚬ A =
+    andThen(introSnd(forevert[A]), andThen(assocRL, elimFst(andThen(swap, backvert[-[A]]))))
+
+  /** Double-inversion introduction. */
+  def dii[A]: A -⚬ -[-[A]] =
+    andThen(introFst(forevert[-[A]]), andThen(assocLR, elimSnd(andThen(swap, backvert[A]))))
 }
