a541fd3c6db6ab826db4b0dd976c8d327b45a077
Tomas Mikula <tomas.mikula@gmail.com>
2021-09-28 13:51:34 +0200

    Fix docs directory.

--- scripts/publish-docs-latest.sh
+++ scripts/publish-docs-latest.sh
@@ -3,7 +3,6 @@ set -x  # echo commands as they are executed
 
 sbt docs/docsSite
 git rm -rf --ignore-unmatch docs/latest
-mkdir -p docs/latest
 cp -R docs-project/target/docs-site docs/latest
 git add docs/latest
-git commit
+git commit docs/latest
