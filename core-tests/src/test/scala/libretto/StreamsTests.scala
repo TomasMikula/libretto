b2170b547ce400e99225bd635161e7ad4a1b99d2
Tomas Mikula <tomas.mikula@gmail.com>
2022-08-05 23:16:49 +0200

    Migrate StreamsTests to the new testing framework.

--- core-tests/src/test/scala/libretto/StreamsTests.scala
+++ core-tests/src/test/scala/libretto/StreamsTests.scala
@@ -1,107 +1,116 @@
 package libretto
 
-import libretto.testing.scalatest.TestSuite
+import libretto.testing.{ScalaTestKit, TestCase, Tests}
+import libretto.testing.scalatest.ScalatestScalaTestSuite
+import libretto.util.Monad.syntax._
 import scala.concurrent.duration._
 
-class StreamsTests extends TestSuite {
-  import kit.dsl._
-  import kit.coreLib._
-  import kit.coreStreams._
-  import kit.scalaLib._
+class StreamsTests extends ScalatestScalaTestSuite {
+  override def testCases(using kit: ScalaTestKit): Tests.Cases[kit.type] = {
+    import kit.{dsl, expectVal}
+    import kit.dsl._
+    import kit.Outcome.assertEquals
 
-  val scalaStreams = ScalaStreams(kit.dsl, kit.coreLib, kit.scalaLib, kit.coreStreams)
+    val coreLib = CoreLib(dsl)
+    val scalaLib = ScalaLib(dsl: dsl.type, coreLib)
+    val coreStreams = CoreStreams(dsl, coreLib)
+    val scalaStreams = ScalaStreams(kit.dsl, coreLib, scalaLib, coreStreams)
 
-  import scalaStreams._
+    import coreLib._
+    import scalaLib._
+    import scalaStreams._
 
-  test("toList ⚬ fromList = id") {
-    assertVal(
-      Pollable.fromList(List(1, 2, 3, 4, 5, 6)) > Pollable.toList,
-      List(1, 2, 3, 4, 5, 6),
-    )
-  }
+    Tests.Cases(
+      "toList ⚬ fromList = id" -> TestCase
+        .interactWith { Pollable.fromList(List(1, 2, 3, 4, 5, 6)) > Pollable.toList }
+        .via { expectVal(_).assertEquals(List(1, 2, 3, 4, 5, 6)) },
 
-  test("Pollable.map") {
-    assertVal(
-      Pollable.fromList(List(1, 2, 3)) > Pollable.map(_.toString) > Pollable.toList,
-      List("1", "2", "3"),
-    )
-  }
+      "Pollable.map" -> TestCase
+        .interactWith {
+          Pollable.fromList(List(1, 2, 3)) > Pollable.map(_.toString) > Pollable.toList
+        }.via {
+          expectVal(_).assertEquals(List("1", "2", "3"))
+        },
 
-  test("partition") {
-    assertVal(
-      Pollable.of(1, 2, 3, 4, 5, 6)
-        .>(Pollable.map { i => if (i % 2 == 0) Left(i) else Right(i) })
-        .>(Pollable.partition)
-        .par(Pollable.toList, Pollable.toList)
-        .>(unliftPair),
-      (List(2, 4, 6), List(1, 3, 5)),
-    )
-  }
+      "partition" -> TestCase
+        .interactWith {
+          Pollable.of(1, 2, 3, 4, 5, 6)
+            .>(Pollable.map { i => if (i % 2 == 0) Left(i) else Right(i) })
+            .>(Pollable.partition)
+            .par(Pollable.toList, Pollable.toList)
+            .>(unliftPair)
+        }.via {
+          expectVal(_).assertEquals((List(2, 4, 6), List(1, 3, 5)))
+        },
 
-  test("concat") {
-    assertVal(
-      forkMap(Pollable.of(1, 2, 3), Pollable.of(4, 5, 6))
-        .>(Pollable.concat)
-        .>(Pollable.toList),
-      List(1, 2, 3 ,4, 5, 6),
-    )
-  }
+      "concat" -> TestCase
+        .interactWith {
+          forkMap(Pollable.of(1, 2, 3), Pollable.of(4, 5, 6))
+            .>(Pollable.concat)
+            .>(Pollable.toList)
+        }.via {
+          expectVal(_).assertEquals(List(1, 2, 3 ,4, 5, 6))
+        },
 
-  test("merge") {
-    assertVal(
-      forkMap(
-        Pollable.of(1, 2, 3),
-        Pollable.of(4, 5, 6),
-      )
-        .>(Pollable.merge)
-        .>(Pollable.toList)
-        .>(mapVal(_.toSet)),
-      Set(1, 2, 3, 4, 5, 6),
-    )
-  }
+      "merge" -> TestCase
+        .interactWith {
+          forkMap(
+            Pollable.of(1, 2, 3),
+            Pollable.of(4, 5, 6),
+          )
+            .>(Pollable.merge)
+            .>(Pollable.toList)
+            .>(mapVal(_.toSet))
+        }.via {
+          expectVal(_).assertEquals(Set(1, 2, 3, 4, 5, 6))
+        },
 
-  test("mergeAll") {
-    testVal(
-      LList1
-        .of(
-          Pollable.of(1, 2, 3),
-          Pollable.of(4, 5, 6) > Pollable.delay(10.millis),
-          Pollable.of(7, 8, 9),
-        )
-        .>(LList1.toLList)
-        .>(Pollable.mergeAll)
-        .>(Pollable.toList)
-    ) { (res: List[Int]) =>
-      assert(res.toSet == Set(1, 2, 3, 4, 5, 6, 7, 8, 9))
-      val (res1, res2) = res.splitAt(6)
-      assert(res1.toSet == Set(1, 2, 3, 7, 8, 9))
-      assert(res2 == List(4, 5, 6))
-    }
-  }
+      "mergeAll" -> TestCase
+        .interactWith {
+          LList1
+            .of(
+              Pollable.of(1, 2, 3),
+              Pollable.of(4, 5, 6) > Pollable.delay(10.millis),
+              Pollable.of(7, 8, 9),
+            )
+            .>(LList1.toLList)
+            .>(Pollable.mergeAll)
+            .>(Pollable.toList)
+        }.via { port =>
+          for {
+            res <- expectVal(port)
+            _   <- assertEquals(res.toSet, Set(1, 2, 3, 4, 5, 6, 7, 8, 9))
+            (res1, res2) = res.splitAt(6)
+            _   <- assertEquals(res1.toSet, Set(1, 2, 3, 7, 8, 9))
+            _   <- assertEquals(res2, List(4, 5, 6))
+          } yield ()
+        },
 
-  test("broadcastByKey") {
-    import Pollable.broadcastByKey
-    import Pollable.BroadcastByKey.{close => closeBroadcast, subscribe}
+      "broadcastByKey" -> TestCase
+        .interactWith {
+          import Pollable.broadcastByKey
+          import Pollable.BroadcastByKey.{close => closeBroadcast, subscribe}
 
-    val byLength: Val[String] -⚬ (Val[Int] |*| Val[String]) =
-      mapVal[String, (Int, String)](s => (s.length, s)) > liftPair
+          val byLength: Val[String] -⚬ (Val[Int] |*| Val[String]) =
+            mapVal[String, (Int, String)](s => (s.length, s)) > liftPair
 
-    val input: Done -⚬ Pollable[String] =
-      Pollable.of("f", "fo", "foo", "fooo", "foooo", "pho", "phoo", "phooo", "bo", "boo")
+          val input: Done -⚬ Pollable[String] =
+            Pollable.of("f", "fo", "foo", "fooo", "foooo", "pho", "phoo", "phooo", "bo", "boo")
 
-    val prg: Done -⚬ Val[List[String]] =
-      input
-        .>(Pollable.delay(10.millis)) // delay so that subscribers have some time to subscribe
-        .>(broadcastByKey(byLength))
-        .>(subscribe(3))
-        .>.fst(subscribe(4))
-        .assocLR.>.snd(Pollable.merge)
-        .par(closeBroadcast, Pollable.toList)
-        .awaitFst
+          val prg: Done -⚬ Val[List[String]] =
+            input
+              .>(Pollable.delay(10.millis)) // delay so that subscribers have some time to subscribe
+              .>(broadcastByKey(byLength))
+              .>(subscribe(3))
+              .>.fst(subscribe(4))
+              .assocLR.>.snd(Pollable.merge)
+              .par(closeBroadcast, Pollable.toList)
+              .awaitFst
 
-    assertVal(
-      prg > mapVal(_.toSet),
-      Set("foo", "fooo", "pho", "phoo", "boo"),
+          prg > mapVal(_.toSet)
+        }.via {
+          expectVal(_).assertEquals(Set("foo", "fooo", "pho", "phoo", "boo"))
+        },
     )
   }
 }
