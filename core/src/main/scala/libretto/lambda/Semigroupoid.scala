9eeeaad0368c2db9808cf4de49792885e5b363c7
Tomas Mikula <tomas.mikula@gmail.com>
2023-01-03 22:46:04 +0100

    Add .to[Y], .from[X] extension methods to Semigroupoid.

--- core/src/main/scala/libretto/lambda/Semigroupoid.scala
+++ core/src/main/scala/libretto/lambda/Semigroupoid.scala
@@ -3,8 +3,14 @@ package libretto.lambda
 trait Semigroupoid[->[_, _]] {
   def andThen[A, B, C](f: A -> B, g: B -> C): A -> C
 
-  extension [A, B, C](f: A -> B) {
-    def >(g: B -> C): A -> C =
+  extension [A, B](f: A -> B) {
+    def >[C](g: B -> C): A -> C =
       andThen(f, g)
+
+    def to[C](using B =:= C): A -> C =
+      summon[B =:= C].substituteCo(f)
+
+    def from[Z](using Z =:= A): Z -> B =
+      summon[Z =:= A].substituteContra[[a] =>> a -> B](f)
   }
 }
