3ea8225bf836d41c8d08376a2bebe91456ba5432
Tomas Mikula <tomas.mikula@gmail.com>
2023-09-28 11:06:53 +0200

    Work in progress on WorkflowInProgress.

--- lambda-examples/src/main/scala/libretto/lambda/examples/workflow/generic/runtime/WorkflowInProgress.scala
+++ lambda-examples/src/main/scala/libretto/lambda/examples/workflow/generic/runtime/WorkflowInProgress.scala
@@ -28,7 +28,7 @@ object WorkflowInProgress {
 
   case class IncompleteImpl[Action[_, _], Val[_], X, Y, A](
     input: Input[Val, X],
-    cont: WIP.Closure[Action, Val, X, Y],
+    cont: Closure[Action, Val, X, Y],
     resultAcc: Capture[**, Value[Val, _], Y, A],
   ) extends Incomplete[Action, Val, A] {
     override def isReducible: Boolean =
@@ -47,27 +47,76 @@ object WorkflowInProgress {
 
   }
 
+  enum PartiallyAppliedAction[Action[_, _], Val[_], A, B]:
+    case Impl[Action[_, _], Val[_], A, X, B](
+      args: Capture[**, Val, A, X],
+      f: Action[X, B],
+    ) extends PartiallyAppliedAction[Action, Val, A, B]
+
+  object PartiallyAppliedAction {
+    def pure[Action[_, _], Val[_], A, B](f: Action[A, B]): PartiallyAppliedAction[Action, Val, A, B] =
+      PartiallyAppliedAction.Impl(Capture.NoCapture(), f)
+  }
+
+  type Closure[Action[_, _], Val[_], A, B] =
+    FlowAST[PartiallyAppliedAction[Action, Val, _, _], A, B]
+
+  object Closure {
+    type Work[Action[_, _], Val[_], A, B] =
+      FlowAST.Work[PartiallyAppliedAction[Action, Val, _, _], A, B]
+
+    type Shuffled[Action[_, _], Val[_]] =
+      libretto.lambda.Shuffled[FlowAST.Work[PartiallyAppliedAction[Action, Val, _, _], _, _], **]
+
+    def ssc[Action[_, _], Val[_]] =
+      summon[libretto.lambda.SymmetricSemigroupalCategory[Closure[Action, Val, _, _], **]]
+
+    def shuffled[Action[_, _], Val[_]]: Shuffled[Action, Val] =
+      FlowAST.shuffled
+
+    def pure[Action[_, _], Val[_], A, B](f: FlowAST[Action, A, B]): Closure[Action, Val, A, B] =
+      f.translate([x, y] => (f: Action[x, y]) => PartiallyAppliedAction.pure[Action, Val, x, y](f))
+
+    def id[Action[_, _], Val[_], A]: Closure[Action, Val, A, A] =
+      FlowAST.Id()
+
+    def swap[Action[_, _], Val[_], A, B]: Closure[Action, Val, A ** B, B ** A] =
+      FlowAST.Swap()
+
+    def assocLR[Action[_, _], Val[_], A, B, C]: Closure[Action, Val, (A ** B) ** C, A ** (B ** C)] =
+      FlowAST.AssocLR()
+
+    def assocRL[Action[_, _], Val[_], A, B, C]: Closure[Action, Val, A ** (B ** C), (A ** B) ** C] =
+      FlowAST.AssocRL()
+
+    def fst[Action[_, _], Val[_], A, B, Y](f: Closure[Action, Val, A, B]): Closure[Action, Val, A ** Y, B ** Y] =
+      ssc.fst(f)
+
+    def snd[Action[_, _], Val[_], A, B, X](f: Closure[Action, Val, A, B]): Closure[Action, Val, X ** A, X ** B] =
+      ssc.snd(f)
+
+    def dup[Action[_, _], Val[_], A]: Closure[Action, Val, A, A ** A] =
+      FlowAST.Dup()
+  }
+
   def init[Action[_, _], Val[_], A, B](
     input: Value[Val, A],
     wf: FlowAST[Action, A, B],
   ): WorkflowInProgress[Action, Val, B] =
     IncompleteImpl(
       Input.Ready(input),
-      WIP.Closure.pure(wf),
+      Closure.pure(wf),
       Capture.NoCapture(),
     )
-
-  private type Shuffled[Action[_, _], Val[_]] =
-    libretto.lambda.Shuffled[FlowAST.Work[WIP.PartiallyAppliedAction[Action, Val, _, _], _, _], **]
 
   private def propagateValue[Action[_, _], Val[_], F[_], A, B, C](
     value: Value[Val, A],
     remainingInput: Spine[**, Input[Val, _], F],
-    cont: WIP.Closure[Action, Val, F[A], B],
+    cont: Closure[Action, Val, F[A], B],
     resultAcc: Capture[**, Value[Val, _], B, C],
   ): CrankRes[Action, Val, C] = {
-    given sh: Shuffled[Action, Val] =
-      WIP.Closure.shuffled[Action, Val]
+    given sh: Closure.Shuffled[Action, Val] =
+      Closure.shuffled[Action, Val]
 
     import sh.ChaseFwRes.*
 
@@ -81,7 +130,7 @@ object WorkflowInProgress {
         def go[X, V[_], G[_], W](
           pre: sh.Punched[F, [x] =>> G[V[x]]],
           v: Focus[**, V],
-          f: FlowAST.Work[WIP.PartiallyAppliedAction[Action, Val, *, *], V[X], W],
+          f: Closure.Work[Action, Val, V[X], W],
           g: Focus[**, G],
           post: sh.Shuffled[G[W], B]
         ): CrankRes[Action, Val, C] =
@@ -94,7 +143,7 @@ object WorkflowInProgress {
   }
 
   private def accumulateResult[Action[_, _], Val[_], F[_], G[_], A, B](using
-    sh: Shuffled[Action, Val],
+    sh: Closure.Shuffled[Action, Val],
   )(
     newResult: Value[Val, A],
     resultAcc: Capture[**, Value[Val, _], G[A], B],
