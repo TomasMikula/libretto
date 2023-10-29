c8241aeced83a818da50b2fa4d147390b4063843
Tomas Mikula <tomas.mikula@gmail.com>
2023-10-29 22:46:28 +0100

    Add some utility methods to Focus and Projection.

--- lambda/src/main/scala/libretto/lambda/Focus.scala
+++ lambda/src/main/scala/libretto/lambda/Focus.scala
@@ -1,6 +1,6 @@
 package libretto.lambda
 
-import libretto.lambda.util.{BiInjective, Injective}
+import libretto.lambda.util.{BiInjective, Exists, Injective}
 
 sealed trait Focus[|*|[_, _], F[_]] {
   import Focus.*
@@ -26,7 +26,8 @@ object Focus {
     override def injective(using BiInjective[|*|]): Injective[[x] =>> x] =
       Injective.id
 
-  sealed trait Proper[|*|[_, _], F[_]] extends Focus[|*|, F]
+  sealed trait Proper[|*|[_, _], F[_]] extends Focus[|*|, F]:
+    def provePair[X]: Exists[[P] =>> Exists[[Q] =>> F[X] =:= (P |*| Q)]]
 
   case class Fst[|*|[_, _], F[_], B](i: Focus[|*|, F]) extends Proper[|*|, [x] =>> F[x] |*| B]:
     override def injective(using BiInjective[|*|]): Injective[[x] =>> F[x] |*| B] =
@@ -34,11 +35,17 @@ object Focus {
         override def unapply[X, Y](ev: (F[X] |*| B) =:= (F[Y] |*| B)): Tuple1[X =:= Y] =
           ev match { case BiInjective[|*|](i.injective(ev1), _) => Tuple1(ev1) }
 
+    override def provePair[X]: Exists[[P] =>> Exists[[Q] =>> (F[X] |*| B) =:= (P |*| Q)]] =
+      Exists(Exists(summon))
+
   case class Snd[|*|[_, _], F[_], A](i: Focus[|*|, F]) extends Proper[|*|, [x] =>> A |*| F[x]]:
     override def injective(using BiInjective[|*|]): Injective[[x] =>> A |*| F[x]] =
       new Injective[[x] =>> A |*| F[x]]:
         override def unapply[X, Y](ev: (A |*| F[X]) =:= (A |*| F[Y])): Tuple1[X =:= Y] =
           ev match { case BiInjective[|*|](_, i.injective(ev1)) => Tuple1(ev1) }
+
+    override def provePair[X]: Exists[[P] =>> Exists[[Q] =>> (A |*| F[X]) =:= (P |*| Q)]] =
+      Exists(Exists(summon))
 
   def id[|*|[_, _]]: Focus[|*|, [x] =>> x] =
     Id()
