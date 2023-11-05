bfe63d1a7b48fa48f302cf4a3aff48fc28fd72c6
Tomas Mikula <tomas.mikula@gmail.com>
2023-11-05 13:26:15 +0100

    Upgrade Laika to 1.0.0.

--- project/LaikaMarkdownToHtml.scala
+++ project/LaikaMarkdownToHtml.scala
@@ -1,29 +1,30 @@
 import cats.effect.IO
 import cats.effect.unsafe.implicits.global
 import laika.api.Transformer
+import laika.config.SyntaxHighlighting
 import laika.format.{HTML, Markdown}
+import laika.format.Markdown.GitHubFlavor
 import laika.helium.Helium
 import laika.helium.config.TextLink
-import laika.io.implicits._
-import laika.markdown.github.GitHubFlavor
-import laika.parse.code.SyntaxHighlighting
+import laika.io.syntax._
 import laika.theme.Theme
 
 object LaikaMarkdownToHtml {
-  
+
   def apply(srcDir: String, tgtDir: String): Unit = {
     Transformer
       .from(Markdown)
       .to(HTML)
       .withRawContent // support html content in input markdown documents
-      .using(GitHubFlavor, SyntaxHighlighting)
+      .using(GitHubFlavor)
+      .using(SyntaxHighlighting)
       .parallel[IO]
       .withTheme(
         Helium.defaults
           .site.topNavigationBar(
             homeLink = TextLink.external("https://github.com/TomasMikula/libretto", "GitHub"),
           )
-          .site.fontResources(/* Empty overrides Default! We use our custom CSS for the fonts. */)
+          .site.addFontResources(/* Empty overrides Default! We use our custom CSS for the fonts. */)
           .build
       )
       .build
