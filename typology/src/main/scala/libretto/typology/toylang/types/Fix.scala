c72e146d5e328416fea1e80447cfa024cc5b475c
Tomas Mikula <tomas.mikula@gmail.com>
2024-01-05 20:28:29 +0100

    Make Fix in toy lang an uninhabited type.
    Since it's only used in phantom positions.

--- typology/src/main/scala/libretto/typology/toylang/types/Fix.scala
+++
@@ -1,4 +0,0 @@
-package libretto.typology.toylang.types
-
-/** The usual type-level fixed-point. */
-case class Fix[F[_]](unfix: F[Fix[F]])
--- typology/src/main/scala/libretto/typology/toylang/types/package.scala
+++ typology/src/main/scala/libretto/typology/toylang/types/package.scala
@@ -3,6 +3,9 @@ package libretto.typology.toylang.types
 /** Used as a phantom type representing a reference to a surrounding recursive function. */
 sealed trait RecCall[A, B]
 
+/** Used as a phantom type representing the usual type-level fixed-point. */
+sealed trait Fix[F[_]]
+
 case class ScalaTypeParam(filename: String, line: Int, name: String)
 case class ScalaTypeParams(values: Set[ScalaTypeParam]) {
   require(values.nonEmpty)
