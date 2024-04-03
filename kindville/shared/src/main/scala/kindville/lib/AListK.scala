15f205b24b02bddbea9479b82f6909ab0df3b68e
Tomas Mikula <tomas.mikula@gmail.com>
2024-04-03 23:15:56 +0200

    kindville: A failed attempt at AListK.foldLeft

--- kindville/shared/src/main/scala/kindville/lib/AListK.scala
+++ kindville/shared/src/main/scala/kindville/lib/AListK.scala
@@ -19,12 +19,76 @@ sealed trait AListK[K, F <: AnyKind, A <: AnyKind, B <: AnyKind] {
       AListK.cons[K]
     )
 
-  private type AccBox[G <: AnyKind, X <: AnyKind] =
-    Box[[⋅⋅[_]] =>> [G0[_ <: ⋅⋅[K]], X0 <: ⋅⋅[K]] =>> G0[X0], G :: X :: TNil]
+  // private type AccBox[G <: AnyKind, X <: AnyKind] =
+  //   Box[AccBox.Code, G :: X :: TNil]
+
+  // private object AccBox:
+  //   type Code = [⋅⋅[_]] =>> [G0[_ <: ⋅⋅[K]], X0 <: ⋅⋅[K]] =>> G0[X0]
+
+  //   transparent inline def packer =
+  //     Box.packer[Code]
+
+  //   transparent inline def unpacker =
+  //     Box.unpacker[Code]
+
+  // /** Returns `[G[_]] => (G[A]) => ([X, Y] => (G[X], F[X, Y]) => G[Y]) => G[B]`. */
+  // transparent inline def foldLeft =
+  //   decodeExpr[F :: A :: B :: TNil](
+  //     [⋅⋅[_], F[_ <: ⋅⋅[K], _ <: ⋅⋅[K]], A <: ⋅⋅[K], B <: ⋅⋅[K]] => (
+  //       thiz: AListK[K, F, A, B],
+  //       packG: [G[_ <: ⋅⋅[K]], T <: ⋅⋅[K]] => G[T] => Box[[⋅⋅[_]] =>> [H[_ <: ⋅⋅[K]], X <: ⋅⋅[K]] =>> H[X], G :: T :: TNil],
+  //       unpackG: [G[_ <: ⋅⋅[K]], T <: ⋅⋅[K]] => Box[[⋅⋅[_]] =>> [H[_ <: ⋅⋅[K]], X <: ⋅⋅[K]] =>> H[X], G :: T :: TNil] => G[T],
+  //       liftF: [G[_ <: ⋅⋅[K]]] => ([X <: ⋅⋅[K], Y <: ⋅⋅[K]] => (G[X], F[X, Y]) => G[Y]) =>
+  //         Box[[⋅⋅[_]] =>> [G[_ <: ⋅⋅[K]]] =>> [X <: ⋅⋅[K], Y <: ⋅⋅[K]] => (AccBox[G, X], Elem[K, F, X, Y]) => AccBox[G, Y], G :: TNil]
+  //     ) =>
+  //         [G[_ <: ⋅⋅[K]]] =>
+  //           (ga: G[A]) =>
+  //           (f: [X <: ⋅⋅[K], Y <: ⋅⋅[K]] => (G[X], F[X, Y]) => G[Y]) =>
+  //             // val gaBox = packG[G, A](ga)
+  //             // val ff    = liftF[G](f)
+  //             // val gbBox = thiz.foldLeftBoxed[G](packG[G, A](ga), liftF[G](f))
+  //             unpackG[G, B](thiz.foldLeftBoxed[G](packG[G, A](ga), liftF[G](f)))
+  //   )(
+  //     this,
+  //     AccBox.packer,
+  //     AccBox.unpacker,
+  //     decodeExpr[F :: TNil](
+  //       [⋅⋅[_], F[_ <: ⋅⋅[K], _ <: ⋅⋅[K]]] => (
+  //         pack: [G[_ <: ⋅⋅[K]], A <: ⋅⋅[K]] => G[A] => AccBox[G, A],
+  //         unpack: [G[_ <: ⋅⋅[K]], A <: ⋅⋅[K]] => AccBox[G, A] => G[A],
+  //         unpackElem: [F[_ <: ⋅⋅[K], _ <: ⋅⋅[K]], A <: ⋅⋅[K], B <: ⋅⋅[K]] => Elem[K, F, A, B] => F[A, B],
+  //         packF: [G[_ <: ⋅⋅[K]]] => ([X <: ⋅⋅[K], Y <: ⋅⋅[K]] => (AccBox[G, X], Elem[K, F, X, Y]) => AccBox[G, Y]) =>
+  //           Box[[⋅⋅[_]] =>> [G[_ <: ⋅⋅[K]]] =>> [X <: ⋅⋅[K], Y <: ⋅⋅[K]] => (AccBox[G, X], Elem[K, F, X, Y]) => AccBox[G, Y], G :: TNil]
+  //       ) =>
+  //         [G[_ <: ⋅⋅[K]]] => (f: [X <: ⋅⋅[K], Y <: ⋅⋅[K]] => (G[X], F[X, Y]) => G[Y]) =>
+  //           packF[G](
+  //             [X <: ⋅⋅[K], Y <: ⋅⋅[K]] => (gx: AccBox[G, X], fxy: Elem[K, F, X, Y]) =>
+  //               pack(f[X, Y](unpack(gx), unpackElem(fxy)))
+  //           )
+  //     )(
+  //       AccBox.packer,
+  //       AccBox.unpacker,
+  //       Box.unpacker[Elem.Code[K]],
+  //       Box.packer[[⋅⋅[_]] =>> [G[_ <: ⋅⋅[K]]] =>> [X <: ⋅⋅[K], Y <: ⋅⋅[K]] => (AccBox[G, X], Elem[K, F, X, Y]) => AccBox[G, Y]],
+  //     )
+  //   )
+
+  // def foldLeftBoxed[G <: AnyKind](
+  //   ga: AccBox[G, A],
+  //   f:  Box[[⋅⋅[_]] =>> [G[_ <: ⋅⋅[K]]] =>> [X <: ⋅⋅[K], Y <: ⋅⋅[K]] => (AccBox[G, X], Elem[K, F, X, Y]) => AccBox[G, Y], G :: TNil],
+  // ): AccBox[G, B] =
+  //   this match
+  //     case AListK.Empty(ev) =>
+  //       ev match
+  //         case TypeEqK.Refl() => ga
+  //     case cons: AListK.Cons[k, f, a, x, b] =>
+  //       val f1 = Box.unpack(f)
+  //       val gx = f1.polyFunApply[a :: x :: TNil, AccBox[G, x]](ga, cons.head)
+  //       cons.tail.foldLeftBoxed(gx, f)
 }
 
 object AListK {
-  opaque type Elem[K, F <: AnyKind, A <: AnyKind, B <: AnyKind] =
+  type Elem[K, F <: AnyKind, A <: AnyKind, B <: AnyKind] =
     Box[Elem.Code[K], F :: A :: B :: TNil]
 
   private object Elem {
