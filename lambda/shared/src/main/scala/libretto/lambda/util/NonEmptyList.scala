88bdd6ce976ba563fcbe89df1ca958140923dc26
Tomas Mikula <tomas.mikula@gmail.com>
2024-05-19 13:41:55 +0200

    Make NonEmptyList covariant, add `flatMap` and `of(x, ...)`.

--- lambda/shared/src/main/scala/libretto/lambda/util/NonEmptyList.scala
+++ lambda/shared/src/main/scala/libretto/lambda/util/NonEmptyList.scala
@@ -1,10 +1,10 @@
 package libretto.lambda.util
 
-case class NonEmptyList[A](head: A, tail: List[A]) {
-  def ::(a0: A): NonEmptyList[A] =
+case class NonEmptyList[+A](head: A, tail: List[A]) {
+  def ::[A0 >: A](a0: A0): NonEmptyList[A0] =
     NonEmptyList(a0, head :: tail)
 
-  def ++(that: NonEmptyList[A]): NonEmptyList[A] =
+  def ++[A0 >: A](that: NonEmptyList[A0]): NonEmptyList[A0] =
     NonEmptyList(head, tail ::: that.toList)
 
   def toList: List[A] =
@@ -13,6 +13,16 @@ case class NonEmptyList[A](head: A, tail: List[A]) {
   def map[B](f: A => B): NonEmptyList[B] =
     NonEmptyList(f(head), tail.map(f))
 
+  def flatMap[B](f: A => NonEmptyList[B]): NonEmptyList[B] =
+    val NonEmptyList(b0, bs) = f(head)
+    val cs = tail.flatMap(a => f(a).toList)
+    NonEmptyList(b0, bs ::: cs)
+
   def traverse[F[_], B](f: A => F[B])(using F: Applicative[F]): F[NonEmptyList[B]] =
     F.map2(f(head), Applicative.traverseList(tail)(f))(NonEmptyList(_, _))
+}
+
+object NonEmptyList {
+  def of[A](head: A, tail: A*): NonEmptyList[A] =
+    NonEmptyList(head, tail.toList)
 }
