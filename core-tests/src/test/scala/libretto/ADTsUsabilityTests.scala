ae1789a00ebe5e021f5698d32e59a274e2f71114
Tomas Mikula <tomas.mikula@gmail.com>
2024-05-28 23:14:13 +0200

    Test nested patterns.

--- core-tests/src/test/scala/libretto/ADTsUsabilityTests.scala
+++ core-tests/src/test/scala/libretto/ADTsUsabilityTests.scala
@@ -10,7 +10,7 @@ import libretto.testing.scalatest.scaletto.ScalatestScalettoTestSuite
 class ADTsUsabilityTests extends ScalatestScalettoTestSuite {
 
   override def testCases(using kit: ScalettoTestKit): List[(String, TestCase[kit.type])] = {
-    import kit.*
+    import kit.{OutPort as _, *}
     import kit.dsl.*
     import kit.dsl.given
 
@@ -76,6 +76,28 @@ class ADTsUsabilityTests extends ScalatestScalettoTestSuite {
               .end
           }
         }
+
+      def skewLeft[A]: NonEmptyTree[A] -⚬ NonEmptyTree[A] =
+        rec { self =>
+          λ { t =>
+            switch(t)
+              .is { case Leaf(a) => leaf(a) }
+              .is { case Branch(l |*| Leaf(a)) => branch(self(l) |*| leaf(a)) }
+              .is { case Branch(l |*| Branch(rl |*| rr)) => self(branch(branch(l |*| rl) |*| rr)) }
+              .end
+          }
+        }
+
+      def skewRight[A]: NonEmptyTree[A] -⚬ NonEmptyTree[A] =
+        rec { self =>
+          λ { t =>
+            switch(t)
+              .is { case Branch(Branch(ll |*| lr) |*| r) => self(branch(ll |*| branch(lr |*| r))) }
+              .is { case Branch(Leaf(a) |*| r) => branch(leaf(a) |*| self(r)) }
+              .is { case Leaf(a) => leaf(a) }
+              .end
+          }
+        }
     }
 
     object Tree {
@@ -131,6 +153,26 @@ class ADTsUsabilityTests extends ScalatestScalettoTestSuite {
             .is { case NonEmpty(t1) |*| NonEmpty(t2) => nonEmpty(NonEmptyTree.branch(t1 |*| t2)) }
             .end
         }
+
+      def skewLeft[A]: Tree[A] -⚬ Tree[A] =
+        λ { t =>
+          switch(t)
+            .is { case Empty(u) => empty(u) }
+            .is { case NonEmpty(t) => nonEmpty(NonEmptyTree.skewLeft(t)) }
+            .end
+        }
+
+      def skewRight[A]: Tree[A] -⚬ Tree[A] =
+        λ { t =>
+          switch(t)
+            .is { case Empty(u) => empty(u) }
+            .is { case NonEmpty(t) => nonEmpty(NonEmptyTree.skewRight(t)) }
+            .end
+        }
+
+      def print[A](f: A -⚬ Val[String]): Tree[A] -⚬ Val[String] =
+        foldMap(f, unliftPair > mapVal { case (l, r) => s"($l, $r)" })
+          > Maybe.getOrElse(done > constVal("()"))
 
       object Empty {
         def unapply[A](x: $[Tree[A]])(using pos: SourcePos, ctx: LambdaContext): Some[$[One]] =
@@ -193,7 +235,33 @@ class ADTsUsabilityTests extends ScalatestScalettoTestSuite {
             _ <- Outcome.assertEquals(s, "1,2,3,4")
           } yield ()
         }
-    }
+
+    } ++ List(
+      "skewLeft, skewRight" -> TestCase
+        .interactWith {
+          import NonEmptyTree.{leaf, branch}
+
+          λ { case +(d) =>
+            val tree1, tree2 =
+              Tree.branch(
+                branch(leaf(d :>> constVal(1)) |*| leaf(d :>> constVal(2))) |*|
+                branch(leaf(d :>> constVal(3)) |*| leaf(d :>> constVal(4)))
+              )
+            val s1 = Tree.skewLeft (tree1) :>> Tree.print(mapVal(_.toString))
+            val s2 = Tree.skewRight(tree2) :>> Tree.print(mapVal(_.toString))
+            s1 |*| s2
+          }
+        }
+        .via { port =>
+          val (p1, p2) = OutPort.split(port)
+          for {
+            s1 <- expectVal(p1)
+            s2 <- expectVal(p2)
+            _ <- Outcome.assertEquals(s1, "(((1, 2), 3), 4)")
+            _ <- Outcome.assertEquals(s2, "(1, (2, (3, 4)))")
+          } yield ()
+        }
+    )
   }
 
 }
