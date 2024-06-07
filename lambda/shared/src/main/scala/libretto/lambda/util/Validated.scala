2de196d315c1a213168f10a8691ffdb1aa471c46
Tomas Mikula <tomas.mikula@gmail.com>
2024-06-08 00:04:37 +0200

    Validated: make covariant in error type; add Monad instance.

--- lambda/shared/src/main/scala/libretto/lambda/util/Validated.scala
+++ lambda/shared/src/main/scala/libretto/lambda/util/Validated.scala
@@ -1,6 +1,6 @@
 package libretto.lambda.util
 
-enum Validated[E, A] {
+enum Validated[+E, A] {
   case Valid(value: A)
   case Invalid(errors: NonEmptyList[E])
 
@@ -9,14 +9,14 @@ enum Validated[E, A] {
       case Valid(a)    => Valid(f(a))
       case Invalid(es) => Invalid(es)
 
-  infix def zip[B](that: Validated[E, B]): Validated[E, (A, B)] =
+  infix def zip[E1 >: E, B](that: Validated[E1, B]): Validated[E1, (A, B)] =
     (this, that) match
       case (Valid(a)   , Valid(b)   ) => Valid((a, b))
       case (Valid(_)   , Invalid(fs)) => Invalid(fs)
       case (Invalid(es), Valid(_)   ) => Invalid(es)
       case (Invalid(es), Invalid(fs)) => Invalid(es ++ fs)
 
-  def flatMap[B](f: A => Validated[E, B]): Validated[E, B] =
+  def flatMap[E1 >: E, B](f: A => Validated[E1, B]): Validated[E1, B] =
     this match
       case Valid(a)   => f(a)
       case Invalid(e) => Invalid(e)
@@ -30,5 +30,13 @@ object Validated {
     override def map[A, B](fa: Validated[E, A], f: A => B): Validated[E, B] = fa.map(f)
     override def zip[A, B](fa: Validated[E, A], fb: Validated[E, B]): Validated[E, (A, B)] = fa zip fb
     override def pure[A](a: A): Validated[E, A] = Valid(a)
+  }
+
+  given monad[E]: Monad[Validated[E, _]] with {
+    override def flatMap[A, B](fa: Validated[E, A])(f: A => Validated[E, B]): Validated[E, B] =
+      fa.flatMap(f)
+
+    override def pure[A](a: A): Validated[E, A] =
+      Valid(a)
   }
 }
