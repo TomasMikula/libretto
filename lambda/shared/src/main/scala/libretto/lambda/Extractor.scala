9649d59ddf8a925241fceab9f4aaaf7a14a29b23
Tomas Mikula <tomas.mikula@gmail.com>
2024-07-16 23:05:04 +0200

    Add identity Extractor.

--- lambda/shared/src/main/scala/libretto/lambda/Extractor.scala
+++ lambda/shared/src/main/scala/libretto/lambda/Extractor.scala
@@ -1,5 +1,7 @@
 package libretto.lambda
 
+import libretto.lambda.util.{Applicative, TypeEqK}
+
 trait Extractor[->[_, _], <*>[_, _], T, P] {
   val partitioning: Partitioning[->, <*>, T]
   val partition:    partitioning.Partition[P]
@@ -45,4 +47,39 @@ object Extractor {
     override val partitioning: Partitioning[->, <*>, T],
     override val partition:    partitioning.Partition[P],
   ) extends Extractor[->, <*>, T, P]
+
+  def identity[->[_, _], <*>[_, _], T](using Category[->]): Extractor[->, <*>, T, T] =
+    Extractor(IdPartitioning[->, <*>, T](), summon[T =:= T])
+
+  private class IdPartitioning[->[_, _], <*>[_, _], T](using cat: Category[->]) extends Partitioning[->, <*>, T] {
+
+    override type Partition[P] = T =:= P
+
+    override def reinject[P](p: T =:= P): P -> T =
+      cat.id[P].to[T](using p.flip)
+
+    override def sameAs(that: Partitioning[->, <*>, T]): Option[TypeEqK[this.Partition, that.Partition]] =
+      that match
+        case that1: (IdPartitioning[arr, pr, t] & that.type) =>
+          Some(TypeEqK.refl[this.Partition]): Option[TypeEqK[this.Partition, that1.Partition]]
+        case _ =>
+          None
+
+    override def isTotal[P](p: T =:= P): Option[T -> P] =
+      Some(cat.id[T].to[P](using p))
+
+    override def samePartition[P, Q](p: T =:= P, q: T =:= Q): Option[P =:= Q] =
+      Some(p.substituteCo[[X] =>> X =:= Q](q))
+
+    override def compileAt[F[_], G[_], R](
+      pos: Focus[<*>, F],
+      handle: [X] => (x: T =:= X) => G[F[X] -> R]
+    )(using
+      Applicative[G],
+    ): G[F[T] -> R] =
+      handle[T](summon[T =:= T])
+
+    override def showPartition[P](p: T =:= P): String =
+      "Identity"
+  }
 }
