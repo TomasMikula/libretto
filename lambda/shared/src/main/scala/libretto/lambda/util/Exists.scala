3895cf565b9351f854adc473d158b44e94b9ec95
Tomas Mikula <tomas.mikula@gmail.com>
2025-03-26 15:00:04 +0000

    Rename Exists.Some to Indeed.
    So that it could be imported without clashing with scala.Some.

--- lambda/shared/src/main/scala/libretto/lambda/util/Exists.scala
+++ lambda/shared/src/main/scala/libretto/lambda/util/Exists.scala
@@ -8,12 +8,18 @@ sealed trait ExistsCo[+F[_]] {
 sealed trait Exists[F[_]] extends ExistsCo[F]
 
 object Exists {
-  case class Some[F[_], A](override val value: F[A]) extends Exists[F] {
+  case class Indeed[F[_], A](override val value: F[A]) extends Exists[F] {
     override type T = A
   }
 
   def apply[F0[_], A](fa: F0[A]): Exists[F0] =
-    Some(fa)
+    Indeed(fa)
+
+  @deprecated("Use Indeed", since = "0.3.4")
+  type Some[F[_], A] = Indeed[F, A]
+
+  @deprecated("Use Indeed", since = "0.3.4")
+  transparent inline def Some = Indeed
 }
 
 sealed trait ExistsK[F[_[_]]] {
@@ -22,10 +28,16 @@ sealed trait ExistsK[F[_[_]]] {
 }
 
 object ExistsK {
-  case class Some[F[_[_]], A[_]](override val value: F[A]) extends ExistsK[F] {
+  case class Indeed[F[_[_]], A[_]](override val value: F[A]) extends ExistsK[F] {
     override type T[X] = A[X]
   }
 
   def apply[F0[_[_]], A[_]](fa: F0[A]): ExistsK[F0] =
-    Some(fa)
+    Indeed(fa)
+
+  @deprecated("Use Indeed", since = "0.3.4")
+  type Some[F[_[_]], A[_]] = Indeed[F, A]
+
+  @deprecated("Use Indeed", since = "0.3.4")
+  transparent inline def Some = Indeed
 }
