c3b54d26c22fd0ad25e0d61d71ad6f2d8e4bbade
Tomas Mikula <tomas.mikula@gmail.com>
2026-02-17 23:11:53 +0000

    Use no-arg function instead of unit-arg function when decoding a non-parameterized expr.

--- kindville/shared/src/test/scala/kindville/EncodingTests.scala
+++ kindville/shared/src/test/scala/kindville/EncodingTests.scala
@@ -5,19 +5,19 @@ import org.scalatest.funsuite.AnyFunSuite
 
 class EncodingTests extends AnyFunSuite {
 
-  test("decodeExpr[TNil]([⋅⋅[_]] => (_: Unit) => 0)") {
-    val x = decodeExpr[TNil]([⋅⋅[_]] => (_: Unit) => 0)(())
+  test("decodeExpr[TNil]([⋅⋅[_]] => () => 0)()") {
+    val x = decodeExpr[TNil]([⋅⋅[_]] => () => 0)()
     assert(x == 0)
   }
 
-  test("decodeExpr[List :: TNil]([⋅⋅[_], F[_]] => (_: Unit) => 0)") {
-    val x = decodeExpr[List :: TNil]([⋅⋅[_], F[_]] => (_: Unit) => 0)(())
+  test("decodeExpr[List :: TNil]([⋅⋅[_], F[_]] => () => 0)()") {
+    val x = decodeExpr[List :: TNil]([⋅⋅[_], F[_]] => () => 0)()
     assert(x == 0)
   }
 
-  test("decodeExpr[List :: Int :: TNil]([⋅⋅[_], F[_], A] => (_: Unit) => (fa: F[A]) => fa)") {
+  test("decodeExpr[List :: Int :: TNil]([⋅⋅[_], F[_], A] => () => (fa: F[A]) => fa)()") {
     val id: List[Int] => List[Int] =
-      decodeExpr[List :: Int :: TNil]([⋅⋅[_], F[_], A] => (_: Unit) => (fa: F[A]) => fa)(())
+      decodeExpr[List :: Int :: TNil]([⋅⋅[_], F[_], A] => () => (fa: F[A]) => fa)()
     assert(id(List(1, 2, 3)) == List(1, 2, 3))
   }
 
@@ -33,15 +33,15 @@ class EncodingTests extends AnyFunSuite {
     assert(x == List(1, 2, 3))
   }
 
-  test("decodeExpr[List :: Option :: Int :: TNil]([⋅⋅[_], F[_], G[_], A] => (_: Unit) => (fa: F[A], ga: G[A]) => fa)") {
+  test("decodeExpr[List :: Option :: Int :: TNil]([⋅⋅[_], F[_], G[_], A] => () => (fa: F[A], ga: G[A]) => fa)()") {
     val fst: (List[Int], Option[Int]) => List[Int] =
-      decodeExpr[List :: Option :: Int :: TNil]([⋅⋅[_], F[_], G[_], A] => (_: Unit) => (fa: F[A], ga: G[A]) => fa)(())
+      decodeExpr[List :: Option :: Int :: TNil]([⋅⋅[_], F[_], G[_], A] => () => (fa: F[A], ga: G[A]) => fa)()
     assert(fst(List(1, 2, 3), Some(4)) == List(1, 2, 3))
   }
 
-  test("decodeExpr[List :: Option :: Int :: TNil]([⋅⋅[_], F[_], G[_], A] => (_: Unit) => (fa: F[A], f: F[A] => G[A]) => f(fa))") {
+  test("decodeExpr[List :: Option :: Int :: TNil]([⋅⋅[_], F[_], G[_], A] => () => (fa: F[A], f: F[A] => G[A]) => f(fa))()") {
     val go: (List[Int], List[Int] => Option[Int]) => Option[Int] =
-      decodeExpr[List :: Option :: Int :: TNil]([⋅⋅[_], F[_], G[_], A] => (_: Unit) => (fa: F[A], f: F[A] => G[A]) => f(fa))(())
+      decodeExpr[List :: Option :: Int :: TNil]([⋅⋅[_], F[_], G[_], A] => () => (fa: F[A], f: F[A] => G[A]) => f(fa))()
     assert(go(List(1, 2, 3), _.headOption) == Some(1))
   }
 
