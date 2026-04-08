c36090d3da3d32af501fe2181ed9ea8a9dd97e9b
Tomas Mikula <tomas.mikula@gmail.com>
2026-04-08 01:29:35 +0100

    kindville: Test App on a type constructor with multiple type parameters.

--- kindville/shared/src/test/scala/kindville/lib/AppTests.scala
+++ kindville/shared/src/test/scala/kindville/lib/AppTests.scala
@@ -5,14 +5,31 @@ import org.scalatest.funsuite.AnyFunSuite
 
 class AppTests extends AnyFunSuite {
 
-  test("pack, unpack") {
-    val x: App[* :: TNil, Option, Int :: TNil] =
+  test("pack, unpack of Option[Int]") {
+    val x1: App[kindville.*, Option, Int] =
+      App.pack[kindville.*, Option, Int](Some(1))
+
+    val x2: App[* :: TNil, Option, Int :: TNil] =
       App.pack[* :: TNil, Option, Int :: TNil](Some(1))
 
-    val y: Option[Int] =
+    val y1: Option[Int] =
+      App.unpack(x1)
+
+    val y2: Option[Int] =
+      App.unpack(x2)
+
+    assert(y1 == Some(1))
+    assert(y2 == Some(1))
+  }
+
+  test("pack, unpack of Map[String, Int]") {
+    val x: App[* :: * :: TNil, Map, String :: Int :: TNil] =
+      App.pack[* :: * :: TNil, Map, String :: Int :: TNil](Map("foo" -> 3))
+
+    val y: Map[String, Int] =
       App.unpack(x)
 
-    assert(y == Some(1))
+    assert(y == Map("foo" -> 3))
   }
 
 }
