41f3bfa805808fdd6177acfd115b33184bdc685e
Tomas Mikula <tomas.mikula@gmail.com>
2026-08-02 13:25:34 +0100

    kindville: Test usage of the *same* (coded) type param F both as type constructor (F[A]) and type argument (H[F]).

--- kindville/shared/src/test/scala/kindville/EncodingTests.scala
+++ kindville/shared/src/test/scala/kindville/EncodingTests.scala
@@ -33,4 +33,54 @@ class EncodingTests extends AnyFunSuite {
     assert(go(List(1, 2, 3), _.headOption) == Some(1))
   }
 
+  test("decoding higher kinded type parameters of a polymorphic function") {
+    val f = decode([⋅⋅[_]] => k ?=> [F[_ <: ⋅⋅[* -> *]], A <: ⋅⋅[* -> *]] => (fa: F[A]) => (fa: F[A]))
+    // check that the decoded expression has the expected type
+    f: ([F[_[_]], A[_]] => F[A] => F[A])
+
+    // same as `f`, but with kinds of type params defined differently (but equivalently)0
+    val g = decode([⋅⋅[_]] => k ?=> [F[_[_ <: ⋅⋅[kindville.*]]], A[_ <: ⋅⋅[kindville.*]]] => (fa: F[A]) => fa)
+    // check that the decoded expression has the expected type
+    g: ([F[_[_]], A[_]] => F[A] => F[A])
+
+    // test usage of a type parameter (F) both as a type constructor (F[A]) and as a type argument (H[F])
+    type K = * -> *
+    val h = decode([⋅⋅[_]] => k ?=> [H[_[_ <: ⋅⋅[K]]], F[_ <: ⋅⋅[K]], A <: ⋅⋅[K]] => (hf: H[F], fa: F[A]) => (fa, hf))
+    // check that the decoded expression has the expected type
+    h: ([H[_[_[_]]], F[_[_]], A[_]] => (H[F], F[A]) => (F[A], H[F]))
+
+    case class OfInt[F[_]](value: F[Int])
+    case class OfOption[F[_[_]]](value: F[Option])
+    assert(h(OfOption(OfInt(Option(1))), OfInt(Option(2))) == (OfInt(Some(2)), OfOption(OfInt(Some(1)))))
+  }
+
+  test("decodeT with higher-kinded type arguments, one of which is used both as type argument and as type constructor") {
+    type ● = kindville.*
+
+    trait Functor[F[_]]:
+      extension [A](fa: F[A]) def map[B](f: A => B): F[B]
+    object Functor:
+      given Functor[Option] with
+        extension [A](fa: Option[A]) override def map[B](f: A => B): Option[B] = fa.map(f)
+
+    case class EitherT[L, F[_], R](value: F[Either[L, R]])
+    object EitherT {
+      def liftF[L, F[_]: Functor, A](fa: F[A]): EitherT[L, F, A] = EitherT(fa.map(Right(_)))
+    }
+
+    val mkLiftOptionInt =
+      decodeT[([F[_], A] =>> EitherT[String, F, A]) :: Option :: Int :: TNil]:
+        [⋅⋅[_]] => k ?=> [H[_[_ <: ⋅⋅[●]], _ <: ⋅⋅[●]], F[_ <: ⋅⋅[●]], A <: ⋅⋅[●]] => () =>
+          // note that F is used both as type argument (in H[F, X]) and type constructor (in F[A])
+          (liftF: [X <: ⋅⋅[●]] => F[X] => H[F, X]) => (fa: F[A]) => (liftF(fa): H[F, A])
+
+    // check that the decoded expression has the expected type
+    mkLiftOptionInt: ((liftF: [X] => Option[X] => EitherT[String, Option, X]) => Option[Int] => EitherT[String, Option, Int])
+
+    val liftOptionInt: (Option[Int] => EitherT[String, Option, Int]) =
+      mkLiftOptionInt([X] => EitherT.liftF[String, Option, X](_))
+
+    assert(liftOptionInt(Some(0)).value == Some(Right(0)))
+  }
+
 }
