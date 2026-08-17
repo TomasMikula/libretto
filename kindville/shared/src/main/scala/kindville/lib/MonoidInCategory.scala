92c5cbac3251bf3f071a09415f9ba61a10994632
Tomas Mikula <tomas.mikula@gmail.com>
2026-08-17 21:57:15 +0100

    kindville: Add representation of Applicative as a monoid in functor category with Day convolution as the monoidal product.

--- kindville/shared/src/main/scala/kindville/lib/MonoidInCategory.scala
+++ kindville/shared/src/main/scala/kindville/lib/MonoidInCategory.scala
@@ -54,13 +54,34 @@ type ~>[F[_], G[_]] = [A] => F[A] => G[A]
 type Id[A] = A
 type ∘[F[_], G[_]] = [A] =>> F[G[A]]
 type Const[A] = [X] =>> A
+type ConstF[A] = [F[_]] =>> A
 
 trait Functor[F[_]] {
   def map[A, B](f: A => B): F[A] => F[B]
 }
 
-opaque type Monoid[M]    = MonoidInCategory[     ●, Const[Unit], _ => _, (_, _), Unit, M]
-opaque type Monad [M[_]] = MonoidInCategory[● -> ●, Functor    ,   ~>  ,   ∘   , Id  , M]
+sealed trait Day[F[_], G[_], A]:
+  type X
+  type Y
+  def fx: F[X]
+  def gy: G[Y]
+  def f: (X, Y) => A
+
+object Day:
+  private case class DayImpl[F[_], G[_], A, X0, Y0](fx: F[X0], gy: G[Y0], f: (X0, Y0) => A) extends Day[F, G, A] {
+    override type X = X0
+    override type Y = Y0
+  }
+
+  def apply[F[_], G[_], A, X, Y](fx: F[X], gy: G[Y], f: (X, Y) => A): Day[F, G, A] =
+    DayImpl(fx, gy, f)
+
+type DayConv[F[_], G[_]] = [A] =>> Day[F, G, A]
+
+// normally we would make these traits, but here we want to emphasize they are *just* monoids
+opaque type Monoid[M]         = MonoidInCategory[     ●, Const[Unit] , _ => _, (_, _) , Unit, M]
+opaque type Monad [M[_]]      = MonoidInCategory[● -> ●, Functor     ,   ~>  ,   ∘    , Id  , M]
+opaque type Applicative[F[_]] = MonoidInCategory[● -> ●, ConstF[Unit],   ~>  , DayConv, Id  , F]
 
 object Monoid {
   def apply[M](
@@ -108,5 +129,32 @@ object Monad {
       self.combineK
         .typecheckAs[(M ∘ M) ~> M]
         .apply[A](mma)
+  }
+}
+
+object Applicative {
+  def apply[F[_]](
+    pure: Id ~> F,
+    ap: DayConv[F, F] ~> F,
+  ): Applicative[F] =
+    MonoidInCategory[● -> ●, ConstF[Unit], ~>, DayConv, Id, F](
+      App.packer[● -> ●][ConstF[Unit], F](()),
+      Arrow.packer[● -> ●][~>, Id, F](pure),
+      Box.packer[MonoidInCategory.CombineCode[● -> ●]][~>, DayConv, F](ap),
+    )
+
+  extension [F[_]](self: Applicative[F]) {
+    inline def pure[A](a: A): F[A] =
+      self.unitK
+        .typecheckAs[Id ~> F]
+        .apply[A](a)
+
+    inline def ap[A, B](fab: F[A => B], fa: F[A]): F[B] =
+      self.combineK
+        .typecheckAs[DayConv[F, F] ~> F]
+        .apply[B](Day(fab, fa, (f, a) => f(a)))
+
+    inline def map[A, B](fa: F[A])(f: A => B): F[B] =
+      ap(pure(f), fa)
   }
 }
--- kindville/shared/src/test/scala/kindville/lib/MonoidInCategoryTests.scala
+++ kindville/shared/src/test/scala/kindville/lib/MonoidInCategoryTests.scala
@@ -37,4 +37,38 @@ class MonoidInCategoryTests extends AnyFunSuite {
     assert(monad.flatten[Int](Some(None)) == None)
   }
 
+  test("Applicative[Option] is a monoid in the functor category with Day convolution") {
+    import scala.:: as NonEmptyList
+
+    enum Validated[A]:
+      case Valid(value: A)
+      case Invalid(errors: NonEmptyList[String])
+
+    import Validated.*
+
+    def invalid[A](s: String): Validated[A] = Invalid(NonEmptyList(s, Nil))
+
+    val pure: Id ~> Validated =
+      [A] => (a: A) => Valid(a)
+
+    val ap: DayConv[Validated, Validated] ~> Validated =
+      [A] => (day: Day[Validated, Validated, A]) =>
+        (day.fx, day.gy) match
+          case (Valid(x), Valid(y)) => Valid(day.f(x, y))
+          case (Valid(_), Invalid(fs)) => Invalid(fs)
+          case (Invalid(es), Valid(_)) => Invalid(es)
+          case (Invalid(e :: es), Invalid(fs)) => Invalid(NonEmptyList(e, es ::: fs))
+
+    val applicative = Applicative[Validated](
+      pure = pure,
+      ap = ap,
+    )
+
+    assert(applicative.pure[Int](1) == Valid(1))
+    assert(applicative.ap(Valid((x: Int) => x + 1), Valid(1)) == Valid(2))
+    assert(applicative.ap(Valid((x: Int) => x + 1), invalid("bad")) == invalid("bad"))
+    assert(applicative.ap(invalid("bad"), Valid(1)) == invalid("bad"))
+    assert(applicative.map(Valid(1))(_ + 1) == Valid(2))
+  }
+
 }
