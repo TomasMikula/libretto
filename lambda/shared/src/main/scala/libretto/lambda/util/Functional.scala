3ae7be8da37d19860a5182f689be9dbbda9b182d
Tomas Mikula <tomas.mikula@gmail.com>
2026-09-13 20:21:29 +0100

    Add trait Functional2.

--- lambda/shared/src/main/scala/libretto/lambda/util/Functional.scala
+++ lambda/shared/src/main/scala/libretto/lambda/util/Functional.scala
@@ -3,3 +3,7 @@ package libretto.lambda.util
 trait Functional[F[_, _]] {
   def uniqueOutputType[A, B, C](f: F[A, B], g: F[A, C]): B =:= C
 }
+
+trait Functional2[F[_, _, _]] {
+  def uniqueOutputType[A, B, R, S](r: F[A, B, R], s: F[A, B, S]): R =:= S
+}
